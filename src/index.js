ba71214e7399367d4c8971e28e3f81a0ba8cd3e8
Shubham Singh <shubham88singh@gmail.com>
2020-07-23 23:02:37 +0530

    Addingt he starting scene backgroun

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
-import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import Phaser from 'phaser';
+import TitleScene from './scenes/TitleScene';
+let titleScene = new TitleScene();
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
+  parent: 'phaser-example',
+  width: 640,
+  height: 360,
 };
 
 const game = new Phaser.Game(config);
-
-function preload() {
-  this.load.image("logo", logoImg);
-}
-
-function create() {
-  const logo = this.add.image(400, 150, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
+game.scene.add('TitleScene', titleScene);
+game.scene.start('TitleScene');
---
+++ src/scenes/TitleScene.js
@@ -0,0 +1,17 @@
+import bgimage from '../assets/background.png';
+
+class TitleScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'TitleScene' });
+  }
+  preload() {
+    this.load.image('background_image', bgimage);
+  }
+  create() {
+    let background = this.add.sprite(0, 0, 'background_image');
+    background.setOrigin(0, 0);
+    let title_text = this.add.text(100, 100, 'Pokemon Go ');
+  }
+}
+
+export default TitleScene;
