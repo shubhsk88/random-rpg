09ff0ec553a79f5c023f7db455b6f27581faa5ba
Shubham Singh <shubham88singh@gmail.com>
2020-07-25 08:12:51 +0530

    Adding plugin and animation for the player

--- src/Prefabs/world/Player.js
+++ src/Prefabs/world/Player.js
@@ -9,12 +9,47 @@ class Player extends Prefab {
     this.body.collideWorldBounds = true;
     this.scene.physics.add.collider(this, this.scene.layers.buildings);
     this.cursors = this.scene.input.keyboard.createCursorKeys();
-    console.log(this.scene);
+    const anims = this.scene.anims;
+    anims.create({
+      key: 'front',
+      frames: anims.generateFrameNumbers(this.texture.key, {
+        frames: [0, 4, 8, 12],
+      }),
+      frameRate: 6,
+      repeat: 1,
+    });
+    anims.create({
+      key: 'back',
+      frames: anims.generateFrameNumbers(this.texture.key, {
+        frames: [1, 5, 9, 13],
+      }),
+      frameRate: 6,
+      repeat: 1,
+    });
+    anims.create({
+      key: 'left',
+      frames: anims.generateFrameNumbers(this.texture.key, {
+        frames: [2, 6, 10, 14],
+      }),
+      frameRate: 6,
+      repeat: 1,
+    });
+    anims.create({
+      key: 'right',
+      frames: anims.generateFrameNumbers(this.texture.key, {
+        frames: [3, 7, 11, 15],
+      }),
+      frameRate: 6,
+      repeat: 1,
+    });
+
+    
   }
+
   update() {
     this.body.setVelocity(0);
     let { cursors } = this;
-    console.log(cursors);
+
     if (cursors.left.isDown) {
       this.body.setVelocityX(-100);
     } else if (cursors.right.isDown) {
@@ -24,6 +59,16 @@ class Player extends Prefab {
     } else if (cursors.down.isDown) {
       this.body.setVelocityY(100);
     }
+
+    if (cursors.left.isDown) {
+      this.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+      this.anims.play('right', true);
+    } else if (cursors.up.isDown) {
+      this.anims.play('back', true);
+    } else if (cursors.down.isDown) {
+      this.anims.play('front', true);
+    } else this.anims.stop();
   }
 }
 
---
+++ src/plugins/UserInput.js
@@ -0,0 +1,33 @@
+class UserInput {
+  constructor(scene) {
+    this.scene = scene;
+    this.enabled = false;
+  }
+
+  setInput(userInputData) {
+    this.scene.input.keyboard.removeAllListeners('keydown');
+    this.scene.input.keyboard.removeAllListeners('keyup');
+
+    this.scene.input.keyboard.on('keydown', this.processInput, this);
+    this.scene.input.keyboard.on('keyup', this.processInput, this);
+
+    this.userInputs = userInputData;
+    this.enabled = true;
+  }
+  processInput(event) {
+    if (this.enabled) {
+      let userInput = this.userInputs[event.type][event.key];
+      if (userInput) {
+        let context = undefined;
+        let callbackData = userInput.callback.split('.');
+        if (callbackData[0] === 'scene') {
+          context = this.scene;
+        } else {
+          context = this.scene.prefabs[callbackData[0]];
+        }
+        let method = context[callbackData[1]];
+        method.apply(context, userInput.args);
+      }
+    }
+  }
+}
